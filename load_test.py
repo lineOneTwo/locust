e06f1372ca140afcb7e07579534c22b6304603ee
chailin <903369232@qq.com>
2021-03-03 16:58:13 +0800

    提交

--- load_test.py
+++ load_test.py
@@ -21,15 +21,15 @@ class WebsiteUser(HttpUser):
 #     def index(self):
 #         response = self.client.get("/")
 
-# class MyCustomShape(LoadTestShape):
-#     time_limit = 600
-#     spawn_rate = 20
-#
-#     def tick(self):
-#         run_time = self.get_run_time()
-#
-#         if run_time < self.time_limit:
-#             user_count = round(run_time,-2)
-#             return (user_count, self.spawn_rate)
-#
-#         return None
+class MyCustomShape(LoadTestShape):
+    time_limit = 600
+    spawn_rate = 20
+
+    def tick(self):
+        run_time = self.get_run_time()
+
+        if run_time < self.time_limit:
+            user_count = round(run_time,-2)
+            return (user_count, self.spawn_rate)
+
+        return None
